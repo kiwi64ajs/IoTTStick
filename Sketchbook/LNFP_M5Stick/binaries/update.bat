2a2b00ca3cc57691ec42c70c9fb9fff616d74d00
Alex Shepherd <kiwi64ajs@gmail.com>
2021-03-07 15:36:07 +1300

    removed IoTTStickM5 folder as its no longer relevant added the binaries fodler under the sketch folder with the latest .bin files and the upload tools

---
+++ Sketchbook/LNFP_M5Stick/binaries/update.bat
@@ -0,0 +1,2 @@
+esptool.exe write_flash 0x00010000 LNFP_M5Stick.ino.m5stick_c.bin
+esptool.exe write_flash 0x00290000 LNFP_M5Stick.spiffs.bin
---
+++ Sketchbook/LNFP_M5Stick/binaries/update.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+esptool.py write_flash 0x00010000 LNFP_M5Stick.ino.m5stick_c.bin
+esptool.py write_flash 0x00290000 LNFP_M5Stick.spiffs.bin
